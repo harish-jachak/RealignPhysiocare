d88147e03ce029e1fe1db8754bbf7f845828ace2
Harish Jachak <61112704+harish-jachak@users.noreply.github.com>
2023-01-16 12:03:39 +0530

    Update script.js

--- Face-Detection-JavaScript-master/script.js
+++ Face-Detection-JavaScript-master/script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('https://realignphysiocare.in/Face-Detection-JavaScript-master/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('https://realignphysiocare.in/Face-Detection-JavaScript-master/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('https://realignphysiocare.in/Face-Detection-JavaScript-master/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('https://realignphysiocare.in/Face-Detection-JavaScript-master/models')
 ]).then(startVideo)
 
 function startVideo() {
